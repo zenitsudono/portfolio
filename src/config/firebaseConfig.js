15c83eb9036e97c0394c41435ec4df29045df919
Salmi Abderrahman <salmi05abd@gmail.com>
2026-07-01 01:14:31 +0100

    Configure real Firebase credentials for Firestore datastore

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,12 +2,12 @@
 // Create a Web App, initialize Firestore Database, and paste your credentials below:
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDezwagspYuyLF3slsrL-Y_ikH2vpUan-Y",
+  authDomain: "portfolio-salmi.firebaseapp.com",
+  projectId: "portfolio-salmi",
+  storageBucket: "portfolio-salmi.firebasestorage.app",
+  messagingSenderId: "63080879960",
+  appId: "1:63080879960:web:221edd0a5ea8b075e7772d"
 };
 
 export const isFirebaseConfigured = () => {
